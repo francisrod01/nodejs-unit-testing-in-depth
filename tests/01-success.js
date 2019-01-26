843b08f92a0c70cd4342692ce44784a2ceb4980c
Francis Rodrigues <francis.rod01@outlook.com>
2019-01-26 15:53:40 -0300

    Implemented a simple fail and success test

---
+++ tests/01-success.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Success file will be tested', () => {
+  context('function to be tested', () => {
+    it('should do something', () => {
+      assert.equal(1, 1);
+    });
+  });
+});
