f662726e31cd288e75ddf750c179b4f5782e18e3
Francis Rodrigues <francis.rod01@outlook.com>
2019-02-20 19:42:37 -0300

    Using a "findUserById" test in users test file

--- projects/node-unit-testing/lib/users.test.js
+++ projects/node-unit-testing/lib/users.test.js
@@ -10,10 +10,13 @@ chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
 const users = require('./users');
+const User = require('./models/user');
 
 const sandbox = sinon.createSandbox();
 
 describe('users', () => {
+  let findStub;
+  let sampleArgs;
   let sampleUser;
 
   beforeEach(() => {
@@ -38,6 +41,22 @@ describe('users', () => {
       users.get(null, (err, result) => {
         expect(err).to.exist;
         expect(err.message).to.equal('Invalid user id');
+        done();
+      });
+    });
+
+    it('should call findUserById with id and return result', (done) => {
+      sandbox.restore();
+
+      let stub = sandbox.stub(mongoose.Model, 'findById').yields(null, { name: 'foo' });
+
+      users.get(123, (err, result) => {
+        expect(err).to.not.exist;
+        expect(stub).to.have.been.calledOnce;
+        expect(stub).to.have.been.calledWith(123);
+        expect(result).to.be.a('object');
+        expect(result).to.have.property('name').to.equal('foo');
+
         done();
       });
     });
