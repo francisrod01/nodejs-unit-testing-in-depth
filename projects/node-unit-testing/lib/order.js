24662afbf2d2cb4c8fda4f9dda4ece29ca2a2661
Francis Rodrigues <francis.rod01@outlook.com>
2019-03-06 15:52:11 -0300

    Added some logic to sum values in order constructor

--- projects/node-unit-testing/lib/order.js
+++ projects/node-unit-testing/lib/order.js
@@ -7,5 +7,19 @@ class Order {
     this.createdAt = Date.now();
     this.updatedAt = Date.now();
     this.subtotal = 0;
+
+    for (let item of items) {
+      // console.log(item);
+      this.subtotal += item.price;
+    }
+
+    if (this.subtotal <= 50) {
+      this.shipping = 5;
+    }
+    else {
+      this.shipping = 10;
+    }
+
+    this.total = this.subtotal + this.shipping;
   }
 }
